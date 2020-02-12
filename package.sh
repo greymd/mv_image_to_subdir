12db42e52c72b69de00827f2552f54b1f7e5ca24
greymd <yamadagrep@gmail.com>
2020-02-12 22:32:03 +0000

    Add packages

---
+++ package.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -ue
+THIS_DIR=$(
+  cd "$(dirname "${BASH_SOURCE:-$0}")" && pwd
+)
+readonly THIS_DIR
+{
+  cd "${THIS_DIR}"
+  tar zcvf mv_image_to_subdir.tar.gz bin .tar2package.yml
+  docker run -i greymd/tar2rpm < mv_image_to_subdir.tar.gz > pkg/mv_image_to_subdir.rpm.tmp
+  mv pkg/mv_image_to_subdir.rpm{.tmp,}
+  docker run -i greymd/tar2deb < mv_image_to_subdir.tar.gz > pkg/mv_image_to_subdir.deb.tmp
+  mv pkg/mv_image_to_subdir.deb{.tmp,}
+  rm mv_image_to_subdir.tar.gz
+}
