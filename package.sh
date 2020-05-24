408517973babc471f7d29bed2ea49c3ed3a81f62
Yasuhiro Yamada <yamadagrep@gmail.com>
2020-05-24 16:37:18 +0100

    Update package.sh

--- package.sh
+++ package.sh
@@ -7,9 +7,9 @@ readonly THIS_DIR
 {
   cd "${THIS_DIR}"
   tar zcvf mv_image_to_subdir.tar.gz bin .tar2package.yml
-  docker run -i greymd/tar2rpm < mv_image_to_subdir.tar.gz > pkg/mv_image_to_subdir.rpm.tmp
+  docker run -i greymd/tar2rpm:1.0.1 < mv_image_to_subdir.tar.gz > pkg/mv_image_to_subdir.rpm.tmp
   mv pkg/mv_image_to_subdir.rpm{.tmp,}
-  docker run -i greymd/tar2deb < mv_image_to_subdir.tar.gz > pkg/mv_image_to_subdir.deb.tmp
+  docker run -i greymd/tar2deb:1.0.1 < mv_image_to_subdir.tar.gz > pkg/mv_image_to_subdir.deb.tmp
   mv pkg/mv_image_to_subdir.deb{.tmp,}
   rm mv_image_to_subdir.tar.gz
 }
